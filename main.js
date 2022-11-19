7194dc7c181a5eff151fa38c3635e79c7f0afb5f
Artur <artnazarian@yandex.ru>
2022-11-19 16:11:14 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+let poxos=10*2
+let x=poxos-2
+poxos=poxos-x
+
+// "40" string
+// "john" string
+// 40 Number
+// Boolean (true; false  == ===)
+
+console.log(poxos -10)
+let user_name = "John";
+let user_surname = 'Johnyan';
+let age = `20`;
+
+console.log(user_name);
+console.log(user_surname);                 //console.log(undefined);
+console.log(age);
+console.log("20"+"40");
+console.log(20+40);
+console.log(`Hello ${2+3}`);
+console.log("Hello ${2+3}");
+console.log(`Hello ${user_name} ${user_surname} Your age: ${age} `); //ES6
+
+console.log("Hello \"John\" hfhjfhhhf ");
+
+// console.log("Hello John Johnyan! Your age: 20");
+
+//console.log(5==5);
+//console.log(5=="5");
+//console.log(5==="5");
+
+console.log(5!=5);
+console.log(5!="5");
+console.log(5!=="5");               //(let hamikdzadza=4  hamikdzadza=hamikdzadza+4  hamikdzadza+=4)
+console.log(5>=5);                   //(hamikdzadza++;   hamikdzadza--;  (kavelacni ev kpakasecni 1 ov) )
+console.log(true && false);
+console.log(0 && false);
+console.log(true && 2);
+console.log(5>3 || 5<2);
+console.log(1>3 || 5<2);
+
+
---
+++ main1.js
@@ -0,0 +1,123 @@
+ 
+const a = 3;  
+
+for (i = 1; i<=10; i++) {
+  console.log(` ${a} * ${i} = ${a * i} `);
+}
+
+
+
+
+/*
+const num1 = Number(prompt("mutqagreq arajin tivy"));
+let nshan = prompt("mutqagreq nshane (+ - * /)");
+const num2 = Number(prompt("mutqagreq erkrord tivy"));
+
+   if (nshan === "+") {
+   console.log(`katarvel e gumarum, ardyunqy ${num1 + num2}`);
+ } else if (nshan === "-") {
+   console.log(`katarvel e hanum, ardyunqy ${num1 - num2}`);
+ } else if (nshan === "/") {
+  console.log(`katarvel e bajanum, ardyunqy ${num1 / num2}`);                
+ } else { console.log("anhayt nshan")
+ }
+
+
+
+
+ 
+const num1 = Number(prompt("input number 1"));         
+const num2 = Number(prompt("input number 2"));
+
+ if (num1 > num2) {
+  console.log(`num1 > num2`);                                 (if-եթե) paymani operator
+ } else if (num1 < num2) {
+  console.log(`num1 < num2`);
+ } else   {
+ console.log(`num1 === num2`); 
+ }   
+ 
+  console.log(`test`);
+  console.log("test 2");
+
+
+
+
+ /*                                      
+let k = 0;                  
+let tox = 8;                           
+let res = "";
+let symbol = "*";
+
+ do { 
+ res +=  symbol;   
+console.log(res);
+ k++; 
+ } while (k < tox);
+     
+ 
+
+let ab = "";
+const simbol = "*";
+const tiv = 8;
+
+for (let i = 0; i < tiv; i++) {
+ab += simbol;
+console.log(ab);
+}
+
+
+
+
+ const z = 109;
+const v = 36;
+
+for (let i = v; i < z; i++) {
+console.log(i);
+}
+
+
+
+let res = 1;
+const q = 2;
+const u = 9; 
+
+
+for (let i = 0; i < u; i++) {
+  res *= q;
+    console.log(res);
+}
+*/
+
+/*
+const tox = Number(prompt ("mutqagreq   tiv"));
+const symbol = prompt("symbol");
+
+let a = 0;
+let res = "";
+
+while (a < tox){
+ res += symbol;   
+    console.log(res);
+   a++; 
+} 
+
+
+let y = 0;
+let num = 2;
+let res = 1;
+let c = 12
+
+while (y < c) {
+    res *= num;
+    console.log(res);
+    y++;
+} 
+*/
+
+
+
+
+
+
+
